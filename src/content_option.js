9bf3ce359f9041dcfc6701612bb6fd2cd3eb79ac
shivanis21cs <158559520+shivanis21cs@users.noreply.github.com>
2024-08-20 12:33:34 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,105 @@
+import { FaInstagram } from "react-icons/fa";
+
+const logotext = "REDINTEK";
+const meta = {
+    title: "REDINTEK",
+};
+
+const introdata = {
+    title: "BEST SOLUTION WE OFFER",
+    animated: {
+        first: "Crafting Digital Experiences, One Line of Code at a Time",
+        second: "Innovative Web Solutions for Tomorrow's Business.",
+        third: "Your Vision, Our Code – Building the Future Together",
+    },
+    description: "Redintek is a leading web development agency specializing in creating cutting-edge, user-friendly websites and web applications that drive business growth and deliver exceptional user experiences.",
+    your_img_url: "https://i0.wp.com/picjumbo.com/wp-content/uploads/business-woman-typing-on-macbook.jpg?w=600&quality=80",
+};
+
+const dataabout = {
+    title: "WELCOM TO THE BEST WEBSITE DEVELOPMENT COMPANY IN KOCHI & KOLLAM",
+    aboutme: " Redintek is a Tech Soul for your Business, provides exclusive IT solutions in the areas of Software designing and development. Our purpose is to deliver assurance of technology and human ingenuity.",
+};
+const worktimeline = [{
+        jobtitle: "Quick Support",
+        where: "We provide 24x7 support service to our clients because our team gives importance to support our clients at their needy time We have passionate professionals that manages customer relationships in everything we do",
+    },
+    {
+        jobtitle: "Dedicated Team",
+        where: "We address our clients' business challenges in a customized way so our team comes up with an effective solution to the problem. Redintek always focus on innovative ideas to handle customers issues.",
+    },
+    {
+        jobtitle: "99% Client Satisfaction",
+        where: "Redintek team always maintains the trust of our clients and we provide quality services that always satisfy our customers.",
+    },
+];
+const services = [{
+        title: "We are experienced",
+        description: "We are experienced in providing software solutions across industries and across the globe.",
+    },
+    {
+        title: "We are on Time",
+        description: "Our expert team is always there for our clients to deliver their solutions and services on time",
+    },
+    {
+        title: "We are Satisfactory",
+        description: "Redintek helps you achieve transformational change at scale and speed.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://gvcloudsecure.com/wp-content/uploads/2021/05/Web-App-Development-Services-Providers-in-Noida-01.jpg",
+        description: " We are providing STATIC WEB DESIGN & DYNAMIC WEB DESIGN.",
+       
+    },
+    {
+        img: "https://www.commtech.com/wp-content/uploads/2023/08/Mobile-Technologies.jpg",
+        description: "Robust & Scalable Mobile Apps solutions and Services for ios,Android,Flutter.",
+       
+    },
+    {
+        img: "https://img.freepik.com/premium-vector/digital-marketing-symbols-chart-background_144316-1725.jpg",
+        description: "Why you need Redintek for Digital Marketing is to UPHOLD BRAND VALUE, ESTABLISH YOUR BRAND, BUILD REGULAR TOUCH POINTS ONLINE,INCREASE BRAND APPRECIATION.",
+        
+    },
+    {
+        img: "https://img.lovepik.com/photo/48008/5914.jpg_wh300.jpg",
+        description: "Engage and Entertain for Content & Social Media Strategy,Campaign Creation & management,Facebook & Instagram Marketing,Community Management,Content Creation,Native Advertising",
+       
+    },
+    {
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxJ17GfeyOgtOC57lHY016h2hC9_7fWOSkLw&s",
+        description:"Our team offers Search Engine Optimization and Social Media Marketing services to enhance our customer's business, fostering digital brand growth.",
+    },
+    {
+        img: "https://cdn.shopaccino.com/igmguru/products/cyber-security-course-igmguru_1273766112_l.jpg?v=464",
+        description: "We do vulnarability Assesment for Web&Mobile Application,Network,API,Thick Client and Types of testing is Server Hardening,Server,Certification.",
+    },];
+
+const contactConfig = {
+    YOUR_EMAIL: "info@redintek.com",
+    YOUR_FONE: "(+91)893929 1113",
+    YOUR_LOCATION: "939, Redintek Private Limited Poruvazhy P.O. Kollam, Kerala -690520",
+    description:"Redintek Pvt Ltd, Tower 1, 1E, Skyline Orion Park Edapally, Ernakulam,Kerala-682024 ",
+ 
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    Instagram: "https://www.instagram.com/redintek/",
+    facebook: "https://www.facebook.com/Redintek",
+    linkedin: "https://www.linkedin.com/company/redintek-pvt-ltd/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
